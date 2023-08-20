f7b74252118e5b52113fd04af54c46e5951dd8fd
Robert <snaipertist@gmail.com>
2023-08-20 13:36:12 +0400

    The user click actually responds to the glowing button know. added sound effect when pressed button is wrong

--- index.js
+++ index.js
@@ -16,42 +16,57 @@
 //         setTimeout(() => {$($("#"+ randomItem.id).css("border", "10px solid black"))}, "1000")
 //     }, 2000)
 // }
+let currentLevel = 1;
 
-let currentLevel = 1
-// main user click animation
-function userClick() {
-     $(".btn").click(function(){
-        let music = new Audio("sounds/" + this.id + ".mp3")
+
+//* main user click animation
+function userClick(buttonId) {
+        let music = new Audio("sounds/" + buttonId + ".mp3")
         music.volume = 0.1
         music.play()
-        $("#"+this.id).animate({opacity: "0.5"})
-        setTimeout(() => $("#"+this.id).animate({opacity: 1}))
-        // console.log(this.id)
-        console.log(this.id)
-        return this.id
-    })
-}
-
-// setting animation
-function animations(){
-        btnArray = $(".btn")
-        let randomItem = btnArray[Math.floor(Math.random()*btnArray.length)];
-        $("#"+ randomItem.id).css("border", "10px solid white");
-        setTimeout(() => {$($("#"+ randomItem.id).css("border", "10px solid black"))}, "1000")
-}
-
-// getting pressed keys
+        $("#"+buttonId).animate({opacity: "0.5"})
+        setTimeout(() => $("#"+buttonId).animate({opacity: 1}))
+    }
+//* getting pressed keys
 $(document).keyup(function(event){
-    console.log(event.key)
-    // starting the game
+    //* starting the game
     if (event.key === "a"){
-        // calling user click
-        userClick()
-        // creating lvl difficulty
-        // setting interval buttons will glow every 2 sec and game will end after 8 sec
+        //* setting animation
+        function animations(){
+            btnArray = $(".btn")
+            let randomItemId = btnArray[Math.floor(Math.random()*btnArray.length)].id;
+            $("#"+ randomItemId).css("border", "10px solid white");
+            setTimeout(() => {$($("#"+ randomItemId).css("border", "10px solid black"))}, "1000")
+            //* calling user click
+
+            $(".btn").off("click").on("click", function (){
+                clickedButtonId = this.id;
+                userClick(clickedButtonId);
+                if (clickedButtonId == randomItemId){
+                    console.log("Right button was clicked!")
+                } else {
+                    console.log("wrong button was clicked!")
+                    let music = new Audio("sounds/wrong.mp3")
+                    music.volume = 0.2
+                    music.play()
+                }
+            })
+            //* checking what button user clicked
+    
+        }
+        // $(".btn").off("click").on("click", function (){
+        //     clickedButtonId = this.id;
+        //     userClick(clickedButtonId);
+        // })
+        //* setting interval buttons will glow every 2 sec and game will end after 8 sec
         myInterval = setInterval(animations, 2000);
         setTimeout(() => {
             clearInterval(myInterval)
-        }, 8000);
+        }, 10000000)
+        //* responding user clicks to button glowing
+        //? How to get randomItem.id outer the function and compare it with click id??
+
+        //! creating lvl difficulty
+
     };
 })
